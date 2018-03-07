b4cbf4bc92ce1c1071af2805d694d504cc22b4a1
tkmageztik <jujex_90@hotmail.com>
2018-03-06 22:27:06 -0500

    Add project files.

---
+++ NS.MYS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NS.MYS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
